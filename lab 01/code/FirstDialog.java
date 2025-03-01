0a004ca890cefbc459adfe7fb2b75e38218c04fb
tbl603493 <thaibaole1209@gmail.com>
2025-03-01 11:33:36 +0700

    add code

---
+++ lab 01/code/FirstDialog.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+public class FirstDialog {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"20235663 - Le Thai Bao. Hello world! How are you?");
+        System.exit(0);
+    }
+}
