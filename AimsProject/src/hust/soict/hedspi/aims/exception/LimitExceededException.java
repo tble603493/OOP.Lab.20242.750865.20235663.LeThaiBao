477500abee0811c089c600c1f9c7fde37e12639d
tble603493 <thaibaole1209@gmail.com>
2025-05-30 22:44:56 +0700

    Delete AimsProject directory

--- AimsProject/src/hust/soict/hedspi/aims/exception/LimitExceededException.java
+++
@@ -1,7 +0,0 @@
-package hust.soict.hedspi.aims.exception;
-
-public class LimitExceededException extends Exception {
-    public LimitExceededException(String message) {
-        super(message);
-    }
-}
--- AimsProject/src/hust/soict/hedspi/aims/exception/MediaNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package hust.soict.hedspi.aims.exception;
-
-public class MediaNotFoundException extends Exception {
-	public MediaNotFoundException(String message) {
-        super(message);
-    }
-}
